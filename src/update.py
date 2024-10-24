4bca0f7011c76cf50dea8d7760c36270fe056afc
Alexandre Filippone <144365522+ZakiarV@users.noreply.github.com>
2024-10-24 16:51:26 -0500

    test_update 2

--- src/update.py
+++ src/update.py
@@ -15,9 +15,19 @@ def download_files():
         with tempfile.TemporaryDirectory() as temp_dir:
             subprocess.run(["git", "clone", "https://github.com/ZakiarV/TTWGMCalculator.git", temp_dir], check=True)
             access_files_to_update = os.path.join(temp_dir, "data", "files_to_update.json")
-            with open(access_files_to_update) as f:
+            if not os.path.exists(access_files_to_update):
+                raise FileNotFoundError("files_to_update.json not found")
+            with open(access_files_to_update, "r") as f:
                 files_to_update = json.load(f)
-            print(files_to_update)
+            for file in files_to_update["files_to_update"]:
+                file_path = os.path.join(get_project_directory(), file)
+                if not os.path.exists(file_path):
+                    raise FileNotFoundError(f"{file} not found")
+                if not cmp(file_path, os.path.join(temp_dir, file)):
+                    shutil.copyfile(os.path.join(temp_dir, file), file_path)
+                    print(f"{file} updated")
+                else:
+                    print(f"{file} is up to date")
     except subprocess.CalledProcessError as e:
         print(f"Error: {e}")
         return False
